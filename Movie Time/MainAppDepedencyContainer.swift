bb9c1f281c6d5788b4e819b675b7374b86ace96a
Arpit Singh <ak160399@icloud.com>
2023-01-27 17:37:17 +0000

    Initial Commit

---
+++ Movie Time/MainAppDepedencyContainer.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppDepedencyContainer.swift
+//  Movie Time
+//
+//  Created by Arpit Singh on 27/01/23.
+//
+
+import UIKit
+
+class MainAppDependencyContainer {
+
+    
+    public func makeMainViewController() -> MainViewController {
+        return MainViewController() 
+    }
+
+}
---
+++ Movie Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Time
+//
+//  Created by Arpit Singh on 27/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
